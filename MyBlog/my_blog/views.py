7c5526e55592435f1f847bd271dfb4d56676de19
tidorino <tidorino@hotmail.com>
2023-02-26 18:19:40 +0200

    add '500 and 404 html',500 and 404 view

--- MyBlog/my_blog/views.py
+++ MyBlog/my_blog/views.py
@@ -1,45 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from django.views.generic import TemplateView
 
 from MyBlog.accounts.models import Profile
 from MyBlog.articles.models import Article
 
 
 UserModel = get_user_model()
-
-"""
-{% if user.id == post.author.id %}
-***
-<div class="container">
-<h1>Post</h1>
-{% for post in object_list %}
-{% empty %}
-    <h1>You have not written any posts yet!</h1>
-{% if user.is_authenticated %}
-{% if user.id == post.author.id %}
-    <li><a href="{% url 'app1:article-detail' post.pk %}">{{post.title}}</a> -
-    {{post.author}} - <small>{{post.post_date}}</small> - <small> category : <a href="{% url 'app1:category' post.category|slugify %}">{{post.category}}</a> - </small>
-    <small><a href="{% url 'app1:updatepost' post.pk %}">Edit</a></small><small>
-    <a href="{% url 'app1:deletepost' post.pk %}">- Delete</a>  
-    </small></li>
-    {{post.snippet}}
-    {% endif %}
-    {% endif %}
-{% endfor %}
-</div>
-"""
-"""
-    def get_articles_page(self):
-        return self.request.GET.get('page', 1)
-
-    def get_paginated_articles(self):
-        page = self.get_articles_page()
-        articles = self.object.profile.articles.all()
-        paginator = Paginator(articles, self.articles_paginate_by)
-        return paginator.get_page(page)
-"""
 
 
 def index(request):
@@ -56,21 +23,15 @@ def index(request):
         'profile': profile,
         'no_profile': True,
         'page_object': page_object,
+        # 'liked_posts': liked_posts,
     }
 
     return render(request, 'my_blog/home-page.html', context)
 
 
-# class IndexViewWithTemplate(TemplateView, PermissionRequiredMixin):
-#     template_name = 'my_blog/home-page.html'
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#
-#         # Add specific view stuff, one or more
-#         context['users'] = UserModel.objects.all()
-#         context['posts'] = Article.objects.all()
-#         # context['form'] = MyForm()
-#
-#         # Return the ready-to-use context
-#         return context
+def view_404(request, *args, **kwargs):
+    return render(request, '404.html')
+
+
+def view_500(request, *args, **kwargs):
+    return render(request, '500.html')
--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -3,12 +3,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from MyBlog.my_blog import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('MyBlog.my_blog.urls')),
     path('accounts/', include('MyBlog.accounts.urls')),
     path('articles/', include('MyBlog.articles.urls')),
 ]
+
+handler404 = views.view_404
+handler500 = views.view_500
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
