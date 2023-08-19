6485a1b288a5bc260a5f3c35a1163611e12ba58c
tidorino <teodoratrendafilova81@gmail.com>
2023-08-19 10:41:56 +0300

    has group tag was changed

--- MyBlog/accounts/templatetags/has_group.py
+++ MyBlog/accounts/templatetags/has_group.py
@@ -1,13 +1,8 @@
 from django import template
-from django.contrib.auth.models import Group
 
 register = template.Library()
 
 
 @register.filter(name='has_group')
 def has_group(user, group_name):
-    try:
-        group = Group.objects.get(name=group_name)
-        user.groups.filter(name=group).exists()
-    except Group.DoesNotExist:
-        raise Exception(f"Group '{group_name}' does not exist.")
+    return user.groups.filter(name=group_name).exists()
